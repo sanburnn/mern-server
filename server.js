fb21b20ab9a857015e78f1146cbccb05b035b621
sanburnn <dimas00758@gmail.com>
2024-07-31 19:43:49 +0700

    change port

--- server.js
+++ server.js
@@ -22,5 +22,5 @@ app.use(express.json());
 
 app.use('/api/auth', authRoutes);
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
